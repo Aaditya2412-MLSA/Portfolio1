976b9b15dd313789a8f4f3efb29bc8ef1dc5642e
Aaditya2412-MLSA <158505699+Aaditya2412-MLSA@users.noreply.github.com>
2024-04-26 08:59:23 +0000

    add

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,7 +22,7 @@ import PropTypes from "prop-types";
  */
 import image from "../images/beautysky.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Beautiful Winter Sky";
 
 const Home = ({ name, title }) => {
   return (
@@ -33,7 +33,7 @@ const Home = ({ name, title }) => {
         <h2>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        <img src={arrowSvg} style={{ height: "3rem", width: "3rem", bounce: "1s ease-out"}} alt={imageAltText} />
       </div>
     </section>
   );
